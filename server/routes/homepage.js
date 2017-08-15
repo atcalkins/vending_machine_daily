c7fcb280fdad5d571f6893dcab7215e470f9b6e7
Tyler Calkins <atcalkins@outlook.com>
2017-08-15 10:54:50 -0400

    deleted error from homepage

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -5,12 +5,5 @@ const db = require('../../queries');
 router.get("/", (req, res) => {
   res.render("index", { secondsToRender: Date.now() - req._startTime });
 });
-//
-router.get ("/api/customer/items", db.getAllItems)
-router.post("/api/customer/items/:id/purchases", db.purchaseAnItem)
-// router.get("/api/customer/items/:itemId/purchases", db.getAllPurchases)
-// router.get('/api/vendor/money', db.getMoney)
-// router.post('api/vendor/items', db.addItem)
-// router.put('/api/vendor/items/:itemId', db.updateItem)
 
 module.exports = router;
