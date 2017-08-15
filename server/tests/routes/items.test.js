a7d336209f69619faec658df4c5314e94ab582eb
Tyler Calkins <atcalkins@outlook.com>
2017-08-15 11:41:58 -0400

    Add items route

--- server/tests/routes/items.test.js
+++ server/tests/routes/items.test.js
@@ -8,6 +8,13 @@ describe('Item Router', () => {
         .get("/api/customer/items")
         .expect(200);
     });
+    fit('has a status key in json payload', () => {
+      return request(app)
+        .get("/api/customer/items")
+        .then((res) => {
+          expect(res.body.status).toEqual("success");
+        })
+    })
   });
 });
 // written a failing test (got 404), refactor make it green
